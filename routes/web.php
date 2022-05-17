f8eca58527aea714f38f0687dd44066515b54f3b
Alex Groat <alex.groat.95@gmail.com>
2022-05-17 14:35:06 +0930

    end episode 68

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,14 @@ Route::get('/', function () {
     /**
      * query the posts table where the title and content
      * contain the values of the variables
-     */
+    //  */
 
-    $postTitle = 'delen';
-    $postContent = 'Est provident';
-    $query = DB::table('posts')
-        ->where('title', 'like', "%$postTitle%")
-        ->orWhere('content', 'like', "%$postContent%")
-        ->get();
+    // $postTitle = 'delen';
+    // $postContent = 'Est provident';
+    // $query = DB::table('posts')
+    //     ->where('title', 'like', "%$postTitle%")
+    //     ->orWhere('content', 'like', "%$postContent%")
+    //     ->get();
 
     // dump($query);
 
@@ -33,11 +33,14 @@ Route::get('/', function () {
      * including the title and the content
      */
 
-    $searchTerm = 'Deleniti';
+    $searchTerm = 'Eligendi';
     $query = DB::table('posts')
         // ->where('title', 'like', "%$searchTerm%")
         // ->orWhere('content', 'like', "%$searchTerm%")
-        // laravel doesnt support full text searching, write sql query
+        /* laravel doesnt support full text searching, write sql query 
+            run the following command in tinker
+            DB::statement('ALTER TABLE posts ADD FULLTEXT fulltext_index_for_posts(title,content)')
+        */
         ->whereRaw(
             "MATCH(title, content) AGAINST(? IN BOOLEAN MODE)",
             [$searchTerm]
@@ -46,67 +49,67 @@ Route::get('/', function () {
 
     dump($query);
 
-    /* query single records in the database */
+    // /* query single records in the database */
 
-    $postId = 2;
+    // $postId = 2;
 
-    // fetches post with comments and all columns
-    $res = Post::with('comments')->find($postId);
+    // // fetches post with comments and all columns
+    // $res = Post::with('comments')->find($postId);
 
-    $tagId = 1;
+    // $tagId = 1;
 
-    // $result = Tag::with(['posts' => function ($q) {
+    // // $result = Tag::with(['posts' => function ($q) {
+    // //     // only fetch the posts with tags and only the id and title columns
+    // //     $q->select('posts.id', 'posts.title');
+    // // }])->find($tagId);
+
+    // $categoryId = 2;
+
+    // $result = Category::with(['posts' => function ($q) {
     //     // only fetch the posts with tags and only the id and title columns
-    //     $q->select('posts.id', 'posts.title');
-    // }])->find($tagId);
+    //     $q->select('posts.id', 'posts.title', 'posts.category_id');
+    // }])->find($categoryId);
 
-    $categoryId = 2;
+    // $categories = Category::select('id', 'title')->orderBy('title')->get();
 
-    $result = Category::with(['posts' => function ($q) {
-        // only fetch the posts with tags and only the id and title columns
-        $q->select('posts.id', 'posts.title', 'posts.category_id');
-    }])->find($categoryId);
+    // $mostPopularCategories = Category::select('id', 'title')
+    //     ->withCount('comments')
+    //     ->orderBy('comments_count', 'desc')
+    //     ->take(3)->get();
 
-    $categories = Category::select('id', 'title')->orderBy('title')->get();
+    // $tags = Tag::select('id', 'name')->orderByDesc(
+    //     DB::table(
+    //         'post_tag'
+    //     )
+    //         ->selectRaw('count(tag_id) as tag_count')
+    //         ->whereColumn('tags.id', 'post_tag.tag_id')
+    //         ->orderBy('tag_count', 'desc')
+    //         ->limit(1)
+    // )->get();
+    // /* get all the tags by id and name, order descending by tag count 
+    // we count the same tag name by using the where column constraint, where the id column 
+    // of the tags table is the same as the tag id column in the post_tag pivot table  */
 
-    $mostPopularCategories = Category::select('id', 'title')
-        ->withCount('comments')
-        ->orderBy('comments_count', 'desc')
-        ->take(3)->get();
+    // $latestPosts = Post::select('id', 'title')->latest()->take(5)->withCount('comments')->get();
 
-    $tags = Tag::select('id', 'name')->orderByDesc(
-        DB::table(
-            'post_tag'
-        )
-            ->selectRaw('count(tag_id) as tag_count')
-            ->whereColumn('tags.id', 'post_tag.tag_id')
-            ->orderBy('tag_count', 'desc')
-            ->limit(1)
-    )->get();
-    /* get all the tags by id and name, order descending by tag count 
-    we count the same tag name by using the where column constraint, where the id column 
-    of the tags table is the same as the tag id column in the post_tag pivot table  */
+    // $mostPopularPosts = Post::select('id', 'title')->orderByDesc(
+    //     Comment::selectRaw('count(post_id) as comment_count')
+    //         // where post id column is the same as post id on the comments table
+    //         ->whereColumn('posts.id', 'comments.post_id')
+    //         ->orderBy('comment_count', 'desc')
+    //         ->limit(1)
+    // )->take(5)->withCount('comments')->get();
 
-    $latestPosts = Post::select('id', 'title')->latest()->take(5)->withCount('comments')->get();
+    // $posts = Post::select('id', 'title', 'content')->get();
 
-    $mostPopularPosts = Post::select('id', 'title')->orderByDesc(
-        Comment::selectRaw('count(post_id) as comment_count')
-            // where post id column is the same as post id on the comments table
-            ->whereColumn('posts.id', 'comments.post_id')
-            ->orderBy('comment_count', 'desc')
-            ->limit(1)
-    )->take(5)->withCount('comments')->get();
+    // $comments = Comment::select('id', 'content')->get();
 
-    $posts = Post::select('id', 'title', 'content')->get();
-
-    $comments = Comment::select('id', 'content')->get();
-
-    $mostActiveUsers = User::select('id', 'name')->orderByDesc(
-        Post::selectRaw('count(user_id) as post_count')
-            ->whereColumn('user_id', 'posts.user_id')
-            ->orderBy('post_count', 'desc')
-            ->limit(1)
-    )->take(5)->withCount('posts')->get();
+    // $mostActiveUsers = User::select('id', 'name')->orderByDesc(
+    //     Post::selectRaw('count(user_id) as post_count')
+    //         ->whereColumn('user_id', 'posts.user_id')
+    //         ->orderBy('post_count', 'desc')
+    //         ->limit(1)
+    // )->take(5)->withCount('posts')->get();
 
     // dump($query);
 
